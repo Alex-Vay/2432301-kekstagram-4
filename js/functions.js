42933711552925e15c95c6be2873630a5cda99d9
Алексей Ватлин <vatlin04@mail.ru>
2023-09-28 19:04:59 +0500

    function task

---
+++ js/functions.js
@@ -0,0 +1,22 @@
+const compareLineLength = (line, maxLength) => (line.length <= maxLength);
+
+const checkPalindrome = (line) => {
+  const normalizedLine = line.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < normalizedLine.length/2; i++) {
+    if (normalizedLine[i] !== normalizedLine[normalizedLine.length-1-i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getNumberFromLine = (line) => {
+  let collectedNumber = '';
+  line = line.toString();
+  for (let i = 0; i < line.length; i++) {
+    if (!Number.isNaN(parseInt(line[i], 10))) {
+      collectedNumber += line[i];
+    }
+  }
+  return parseInt(collectedNumber, 10);
+};
